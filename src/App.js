6b44dd7c8b063174b0ee8b5ebc08cce15fbae034
JunhoYoon95 <enffl95@icloud.com>
2022-04-27 17:29:46 +0900

    component 에서 다루는 props, 즉 properties 에 대해서 다루어 본 코드입니다

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import Hello from './components/Hello';
 function App() {
   return (
     <div className='App'>
-      <Hello></Hello>
-      <Hello></Hello>
-      <Hello></Hello>
+      <Hello age={10}></Hello>
+      <Hello age={20}></Hello>
+      <Hello age={30}></Hello>
       {/* Hello 컴포넌트를 3개를 두었습니다. 같은 것 같지만, 각각의 컴포넌트는 서로에게 영향을 주지 않습니다. */}
     </div>
   );
--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,20 +1,26 @@
 import { useState } from "react";
 
-export default function Hello() {
+export default function Hello(props) {
+    // Hello({age}) 이런 식으로 바로 안의 age를 받을 수도 있는 거임
 
+    // 넘겨받은 것을 변경을 할 수는 없어 age 라는 변수에다가 넣어서 변경을 해 볼 것입니다
+    console.log(props)
     const [name, setName] = useState('Junho');
     // name 은 변수, setName 은 이를 변경해주는 함수 명이라고 생각을 하면 될 것 같습니다
+    const [age, setAge] = useState(props.age);
 
     function changeName() {
         setName(name === "Junho" ? "준호" : "Junho")
+        setAge(age + 1)
     }
+
+    
     
     return (
         <div>
-            <h1>State of React</h1>
-            <h2>컴포넌트의 속성값</h2>
+            <h1>Props : properties</h1>
             <h3 id="name">
-                {name}
+                {name}({age})
             </h3>
             <button onClick={changeName}>Change</button>
         </div>
