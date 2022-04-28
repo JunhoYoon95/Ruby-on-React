5655877f3ead45907f935f210b556e0f8117daad
JunhoYoon95 <enffl95@icloud.com>
2022-04-28 16:30:50 +0900

    REST API를 가지고 GET 을 해 보았습니다. 가져오면서 해당 API 문을 hooks 라는 폴더에 넣어 나만의 커스텀 훅으로 사용해 보았습니다. 이제 다음 번에는 update 를 해보려고 합니다

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,20 +1,11 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import useFetch from "../hooks/useFetch";
 import Word from "./Word";
 export default function Day() {
     // const day = useParams().day;  이렇게 해도 무방하니까 안헷갈리게 이렇게 하는게 좋을 듯 싶은디
     const { day }  = useParams();
-    const [words, setWords] = useState([]);
 
-    useEffect(() => {
-        fetch(`http://localhost:3001/words?day=${day}`)
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setWords(data)
-        })
-    },[day])
+    const words = useFetch(`http://localhost:3001/words?day=${day}`)
     
     return <>
     <h2>Day {day}</h2>
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,20 +1,22 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import useFetch from '../hooks/useFetch';
 
 export default function DayList(){
-    const [days, setDays] = useState([]); // days 라는 state  를 만든다. 
-    //처음에는 빈 배열로 만든다. 이후 API 에서 리스트를 가져와서 바꿔주는 방식을 선택하도록 하겠다. 그러면 데이터가 바뀌면 자동으로 렌더링이 될 것이다.
+    const days = useFetch("http://localhost:3001/days");
+    // const [days, setDays] = useState([]); // days 라는 state  를 만든다. 
+    // //처음에는 빈 배열로 만든다. 이후 API 에서 리스트를 가져와서 바꿔주는 방식을 선택하도록 하겠다. 그러면 데이터가 바뀌면 자동으로 렌더링이 될 것이다.
 
-    useEffect(() => {
-        // API 이용합니다
-        fetch('http://localhost:3001/days') // 이렇게 하면 promise가 반환이 됩니다
-        .then(res => {
-            return res.json()   // response는 http응답이고 실제 json 이 아니므로 json으로 변환하여 promise를 반환
-        })
-        .then(data => {
-            setDays(data)
-        })
-    }, []) // 이렇게 count를 따로 넣으면 count에 변화가 있을 때 마다 하는 것이 되는 것인 의존성 배열.
+    // useEffect(() => {
+    //     // API 이용합니다
+    //     fetch('http://localhost:3001/days') // 이렇게 하면 promise가 반환이 됩니다
+    //     .then(res => {
+    //         return res.json()   // response는 http응답이고 실제 json 이 아니므로 json으로 변환하여 promise를 반환
+    //     })
+    //     .then(data => {
+    //         setDays(data)
+    //     })
+    // }, []) // 이렇게 count를 따로 넣으면 count에 변화가 있을 때 마다 하는 것이 되는 것인 의존성 배열.
 
     return (
         <ul className='list_day'>
