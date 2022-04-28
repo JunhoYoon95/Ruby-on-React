f2455096552d8c5ff46fd7f89371636edbaceb34
JunhoYoon95 <enffl95@icloud.com>
2022-04-28 16:17:34 +0900

    이제 dummy 데이터를 일일히 가져오는 것이 아닌 REST API 로 가져오는 방법을 사용해 보았습니다. 이후에는 이 부분을 따로 빼서 재사용성을 높여, 같은 작업을 하는 api 를 공용으로 사용해 보겠습니다

--- src/components/Day.js
+++ src/components/Day.js
@@ -1,18 +1,26 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import dummy from "../db/data.json"
 import Word from "./Word";
 export default function Day() {
     // const day = useParams().day;  이렇게 해도 무방하니까 안헷갈리게 이렇게 하는게 좋을 듯 싶은디
     const { day }  = useParams();
-    const wordList = dummy.words.filter(word => (
-        word.day === Number(day)
-    ))
+    const [words, setWords] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/words?day=${day}`)
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setWords(data)
+        })
+    },[day])
     
     return <>
     <h2>Day {day}</h2>
     <table>
         <tbody>
-            {wordList.map(word => (
+            {words.map(word => (
                 <Word word={word} key={word.id}/>
             ))}
         </tbody>
--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,37 +4,25 @@ import { Link } from 'react-router-dom'
 export default function DayList(){
     const [days, setDays] = useState([]); // days 라는 state  를 만든다. 
     //처음에는 빈 배열로 만든다. 이후 API 에서 리스트를 가져와서 바꿔주는 방식을 선택하도록 하겠다. 그러면 데이터가 바뀌면 자동으로 렌더링이 될 것이다.
-    const [count, setCount] = useState(0);
-
-    function onClick() {
-        setCount(count+1);
-    }
-    function onClick2() {
-        setDays([
-            ...days,
-            {
-                id : Math.random(),
-                day: 1
-            }
-        ])
-    }
 
     useEffect(() => {
-        console.log("Count change"); // 랜더링 결과가 돔에 반영된 직후에 찍힙니다.
-    }, [count]) // 이렇게 count를 따로 넣으면 count에 변화가 있을 때 마다 하는 것이 되는 것인 의존성 배열.
+        // API 이용합니다
+        fetch('http://localhost:3001/days') // 이렇게 하면 promise가 반환이 됩니다
+        .then(res => {
+            return res.json()   // response는 http응답이고 실제 json 이 아니므로 json으로 변환하여 promise를 반환
+        })
+        .then(data => {
+            setDays(data)
+        })
+    }, []) // 이렇게 count를 따로 넣으면 count에 변화가 있을 때 마다 하는 것이 되는 것인 의존성 배열.
 
     return (
-        <>
         <ul className='list_day'>
             {days.map(day => (
                 <li key={day.id}>
                     <Link to={`/day/${day.day}`}>Day {day.day}</Link>
-                    {/* 여기서 day.day는 data.json에서 days 안의 day 값을 말하는 것임 */}
                 </li>
             ))}
         </ul>
-        <button onClick={onClick}>{count}</button>
-        <button onClick={onClick2}>Day change</button>
-        </>
     );
 }
